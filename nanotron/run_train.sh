7312ff67009d26645ecefe0454427436f2d1a0c9
Yu Tian <63806966+YuTian8328@users.noreply.github.com>
2024-11-18 14:52:36 +0200

    Update run_train.sh

--- nanotron/run_train.sh
+++ nanotron/run_train.sh
@@ -5,7 +5,7 @@ if [ $SLURM_LOCALID -eq 0 ] ; then
     rocm-smi
 fi
 sleep 2
-echo 1111
+
 # MIOPEN needs some initialisation for the cache as the default location
 # does not work on LUMI as Lustre does not provide the necessary features.
 export MIOPEN_USER_DB_PATH="/tmp/$(whoami)-miopen-cache-$SLURM_NODEID"
